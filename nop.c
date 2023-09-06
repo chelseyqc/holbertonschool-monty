21c50ba1aeb7c9e2ddbc29aff1d6bbebf78a8f75
root <6721@holbertonstudents.com>
2023-09-05 17:33:02 -0700

    style: betty

--- nop.c
+++ nop.c
@@ -8,7 +8,7 @@
 * Return: void
 */
 void nop(__attribute__((unused))stack_t **stack,
-     __attribute__((unused))unsigned int line_number)
+		__attribute__((unused))unsigned int line_number)
 {
 	;
 }
